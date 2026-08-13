0e13c84d250e5fe9ae12fa542f51fcbb4580e070
Kanishk Singh <singhkanishk2005@gmail.com>
2026-08-13 19:40:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Triplet Sum in Array/triplet-sum-in-array.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean hasTripletSum(int arr[], int target) {
+        int n = arr.length;
+        if (n < 3) return false;
+        
+        Arrays.sort(arr);
+        
+        for (int i = 0; i < n - 2; i++) {
+            int left = i + 1;
+            int right = n - 1;
+            
+            while (left < right) {
+                int sum = arr[i] + arr[left] + arr[right];
+                
+                if (sum == target) {
+                    return true;
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        
+        return false;
+    }
+}
+
